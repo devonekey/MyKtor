17a6681f1f2b49c85e4756f9b7e54ee903228b69
OneKey <devonekey@gmail.com>
2024-10-25 12:29:56 +0900

    feat: '/content/sample.html' 요청이 들어오면 'resources'에 생성한 Static content를 보여주도록 구현

--- src/main/kotlin/com/example/plugins/Routing.kt
+++ src/main/kotlin/com/example/plugins/Routing.kt
@@ -2,12 +2,15 @@ package com.example.plugins
 
 import io.ktor.http.ContentType
 import io.ktor.server.application.Application
+import io.ktor.server.http.content.staticResources
 import io.ktor.server.response.respondText
 import io.ktor.server.routing.get
 import io.ktor.server.routing.routing
 
 fun Application.configureRouting() {
     routing {
+        staticResources("/content", "mycontent")
+
         get("/") {
             call.respondText("Hello Ktor!", ContentType.Text.Plain)
         }
