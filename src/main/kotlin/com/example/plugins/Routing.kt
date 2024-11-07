5e640602287583938ff08d966f820006a7599438
OneKey <devonekey@gmail.com>
2024-11-07 14:10:46 +0900

    feat: 루트('/') GET 요청이 들어오면 간단한 문구를 출력하도록 구현

--- src/main/kotlin/com/example/plugins/Routing.kt
+++ src/main/kotlin/com/example/plugins/Routing.kt
@@ -1,9 +1,15 @@
 package com.example.plugins
 
+import io.ktor.http.ContentType
 import io.ktor.server.application.Application
+import io.ktor.server.response.respondText
+import io.ktor.server.routing.get
 import io.ktor.server.routing.routing
 
 fun Application.configureRouting() {
     routing {
+        get("/") {
+            call.respondText("Hello Ktor!", ContentType.Text.Plain)
+        }
     }
 }
