c92dd2f072023b6ad37162945ca56e6c17cc804f
OneKey <devonekey@gmail.com>
2024-11-12 14:16:32 +0900

    feat: Task Repository 생성

---
+++ src/main/kotlin/com/example/model/TaskRepository.kt
@@ -0,0 +1,10 @@
+package com.example.model
+
+object TaskRepository {
+    val tasks = mutableListOf(
+        Task("cleaning", "Clean the house", Priority.Low),
+        Task("gardening", "Mow the lawn", Priority.Medium),
+        Task("shopping", "Buy the groceries", Priority.High),
+        Task("painting", "Paint the fence", Priority.Medium)
+    )
+}
