ef937b78213cfa97010290075078a2711165c6cc
OneKey <devonekey@gmail.com>
2024-11-14 16:15:53 +0900

    feat: '/tasks/byPriority/{priority}' GET 요청이 들어오면 TaskRepository로부터 priority에 알맞는 Task를 가져오도록 구현

--- src/main/kotlin/com/example/plugins/Routing.kt
+++ src/main/kotlin/com/example/plugins/Routing.kt
@@ -1,12 +1,15 @@
 package com.example.plugins
 
+import com.example.model.Priority
 import com.example.model.TaskRepository
 import com.example.model.taskAsTable
 import io.ktor.http.ContentType
+import io.ktor.http.HttpStatusCode
 import io.ktor.server.application.Application
 import io.ktor.server.application.install
 import io.ktor.server.http.content.staticResources
 import io.ktor.server.plugins.statuspages.StatusPages
+import io.ktor.server.response.respond
 import io.ktor.server.response.respondText
 import io.ktor.server.routing.get
 import io.ktor.server.routing.routing
@@ -40,6 +43,34 @@ fun Application.configureRouting() {
                 contentType = ContentType.parse("text/html"),
                 text = TaskRepository.allTask().taskAsTable()
             )
+        }
+
+        get("/tasks/byPriority/{priority}") {
+            val priorityAsText = call.parameters["priority"]
+
+            if (priorityAsText.isNullOrBlank()) {
+                call.respond(HttpStatusCode.BadRequest)
+
+                return@get
+            }
+
+            try {
+                val priority = Priority.valueOf(priorityAsText)
+                val tasks = TaskRepository.taskByPriority(priority)
+
+                if (tasks.isEmpty()) {
+                    call.respond(HttpStatusCode.NotFound)
+
+                    return@get
+                }
+
+                call.respondText(
+                    contentType = ContentType.Text.Html,
+                    text = tasks.taskAsTable()
+                )
+            } catch (_: IllegalArgumentException) {
+                call.respond(HttpStatusCode.BadRequest)
+            }
         }
     }
 }
