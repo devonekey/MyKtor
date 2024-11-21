58159ac7268a00a38506c71746ff047174fdb5fe
OneKey <devonekey@gmail.com>
2024-11-21 15:40:02 +0900

    feat: 객체 직렬화를 활성화하도록 구현

--- src/main/kotlin/com/example/Application.kt
+++ src/main/kotlin/com/example/Application.kt
@@ -1,11 +1,13 @@
 package com.example
 
 import com.example.plugins.configureRouting
+import com.example.plugins.configureSerialization
 import io.ktor.server.application.Application
 import io.ktor.server.netty.EngineMain
 
 fun main(args: Array<String>) = EngineMain.main(args)
 
 fun Application.module() {
+    configureSerialization()
     configureRouting()
 }
