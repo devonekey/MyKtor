374493348190fc133178ad375410df181f673220
OneKey <devonekey@gmail.com>
2024-11-22 14:39:56 +0900

    test: TaskRepository의 기능들이 제대로 동작하는지를 확인하기 위한 테스트 코드 추가

--- src/test/kotlin/com/example/model/TaskRepositoryTest.kt
+++ src/test/kotlin/com/example/model/TaskRepositoryTest.kt
@@ -1,11 +1,16 @@
 package com.example.model
 
+import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import java.lang.IllegalStateException
+import kotlin.test.assertEquals
+import kotlin.test.assertNotNull
 
 class TaskRepositoryTest {
     @BeforeEach
     fun setup() {
-        with(TaskRepository.tasks) {
+        with(TaskRepository.allTask() as MutableList) {
             clear()
             addAll(
                 mutableListOf(
@@ -18,5 +23,38 @@ class TaskRepositoryTest {
                 )
             )
         }
+    }
+
+    @Test
+    fun `TaskRepository로부터 모든 Task들을 가져올 수 있다`() {
+        val tasks = TaskRepository.allTask()
+
+        assertEquals(expected = 6, actual = tasks.size)
+    }
+
+    @Test
+    fun `TaskRepository로부터 특정 우선 순위의 Task들을 가져올 수 있다`() {
+        val lowTasks = TaskRepository.taskByPriority(Priority.Low)
+        val vitalTasks = TaskRepository.taskByPriority(Priority.Vital)
+
+        assertEquals(expected = 2, actual = lowTasks.size)
+        assertEquals(expected = 0, actual = vitalTasks.size)
+    }
+
+    @Test
+    fun `TaskRepository로부터 특정 이름을 가진 Task를 가져올 수 있다`() {
+        val task = TaskRepository.taskByName("swimming")
+
+        assertNotNull(task)
+        assertEquals(expected = "Go to the beach", actual = task.description)
+    }
+
+    @Test
+    fun `TaskRepository로부터 Task를 추가할 수 있다`() {
+        Assertions.assertThrows(IllegalStateException::class.java) {
+            TaskRepository.addTask(Task("cleaning", "Clean the code", Priority.Low))
+        }
+        TaskRepository.addTask(Task("repairing", "Repair the car", Priority.Low))
+        assertEquals(expected = 7, actual = TaskRepository.allTask().size)
     }
 }
