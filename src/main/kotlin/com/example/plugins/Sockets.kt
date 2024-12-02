d1428dfe7510669ab2d977b5715570ce10ed69d9
OneKey <devonekey@gmail.com>
2024-12-02 13:57:07 +0900

    feat: '/tasks' 웹 소켓 엔드포인트를 요청하면 시간 차를 두고 각각의 Task들을 응답하도록 구현

--- src/main/kotlin/com/example/plugins/Sockets.kt
+++ src/main/kotlin/com/example/plugins/Sockets.kt
@@ -1,11 +1,18 @@
 package com.example.plugins
 
+import com.example.model.TaskRepository
 import io.ktor.serialization.kotlinx.KotlinxWebsocketSerializationConverter
 import io.ktor.server.application.Application
 import io.ktor.server.application.install
+import io.ktor.server.routing.routing
 import io.ktor.server.websocket.WebSockets
 import io.ktor.server.websocket.pingPeriod
+import io.ktor.server.websocket.sendSerialized
 import io.ktor.server.websocket.timeout
+import io.ktor.server.websocket.webSocket
+import io.ktor.websocket.CloseReason
+import io.ktor.websocket.close
+import kotlinx.coroutines.delay
 import kotlinx.serialization.json.Json
 import kotlin.time.Duration.Companion.seconds
 
@@ -16,5 +23,16 @@ fun Application.configureSockets() {
         timeout = 15.seconds
         maxFrameSize = Long.MAX_VALUE
         masking = false
+    }
+
+    routing {
+        webSocket("/tasks") {
+            for (task in TaskRepository.allTask()) {
+                sendSerialized(task)
+                delay(1.seconds)
+            }
+
+            close(CloseReason(CloseReason.Codes.NORMAL, "All done"))
+        }
     }
 }
