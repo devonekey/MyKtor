a0ca79ac943bc3162b285a6abf5e3ce79c14d2b5
OneKey <devonekey@gmail.com>
2024-12-03 16:59:46 +0900

    feat: '/tasks2' 웹 소켓 엔드포인트를 요청하면 대기하다 추가되는 Task를 응답하도록 구현

--- src/main/kotlin/com/example/plugins/Sockets.kt
+++ src/main/kotlin/com/example/plugins/Sockets.kt
@@ -1,13 +1,16 @@
 package com.example.plugins
 
+import com.example.model.Task
 import com.example.model.TaskRepository
 import io.ktor.serialization.kotlinx.KotlinxWebsocketSerializationConverter
 import io.ktor.server.application.Application
 import io.ktor.server.application.install
 import io.ktor.server.routing.routing
 import io.ktor.server.websocket.DefaultWebSocketServerSession
+import io.ktor.server.websocket.WebSocketServerSession
 import io.ktor.server.websocket.WebSockets
 import io.ktor.server.websocket.pingPeriod
+import io.ktor.server.websocket.receiveDeserialized
 import io.ktor.server.websocket.sendSerialized
 import io.ktor.server.websocket.timeout
 import io.ktor.server.websocket.webSocket
@@ -15,6 +18,7 @@ import io.ktor.websocket.CloseReason
 import io.ktor.websocket.close
 import kotlinx.coroutines.delay
 import kotlinx.serialization.json.Json
+import java.util.Collections
 import kotlin.time.Duration.Companion.seconds
 
 fun Application.configureSockets() {
@@ -27,9 +31,27 @@ fun Application.configureSockets() {
     }
 
     routing {
+        val sessions =
+            Collections.synchronizedList<WebSocketServerSession>(ArrayList())
+
         webSocket("/tasks") {
             sendAllTasks()
             close(CloseReason(CloseReason.Codes.NORMAL, "All done"))
+        }
+
+        webSocket("/tasks2") {
+            sessions.add(this)
+            sendAllTasks()
+
+            while (true) {
+                val newTask = receiveDeserialized<Task>()
+
+                TaskRepository.addTask(newTask)
+
+                for (session in sessions) {
+                    session.sendSerialized(newTask)
+                }
+            }
         }
     }
 }
