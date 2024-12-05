d8acae6c4ec51fa82d9a18f358b9c4e8230d9dce
OneKey <devonekey@gmail.com>
2024-12-05 18:21:45 +0900

    test: '/tasks' 웹 소켓 엔드포인트 요청에 대한 테스트 코드 추가

--- src/test/kotlin/com/example/plugins/WebSocketEndpointTest.kt
+++ src/test/kotlin/com/example/plugins/WebSocketEndpointTest.kt
@@ -1,3 +1,59 @@
 package com.example.plugins
 
-class WebSocketEndpointTest
+import com.example.model.Task
+import com.example.model.TaskRepository
+import com.example.module
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.plugins.websocket.WebSockets
+import io.ktor.client.plugins.websocket.converter
+import io.ktor.client.plugins.websocket.webSocket
+import io.ktor.serialization.kotlinx.KotlinxWebsocketSerializationConverter
+import io.ktor.serialization.kotlinx.json.json
+import io.ktor.server.testing.testApplication
+import io.ktor.util.reflect.typeInfo
+import kotlinx.coroutines.flow.consumeAsFlow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.scan
+import kotlinx.serialization.json.Json
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+class WebSocketEndpointTest {
+    @Test
+    fun testRoot() = testApplication {
+        application { module() }
+
+        val client = createClient {
+            install(ContentNegotiation) {
+                json(Json {
+                    prettyPrint = true
+                    isLenient = true
+                    ignoreUnknownKeys = true
+                })
+            }
+            install(WebSockets) {
+                contentConverter = KotlinxWebsocketSerializationConverter(Json)
+            }
+        }
+        val expectedTasks = TaskRepository.allTask()
+        var actualTasks = emptyList<Task>()
+
+        client.webSocket("/tasks") {
+            incoming
+                .consumeAsFlow()
+                .map {
+                    converter!!.deserialize(
+                        charset = Charsets.UTF_8,
+                        typeInfo = typeInfo<Task>(),
+                        content = it
+                    ) as Task
+                }
+                .scan(emptyList<Task>()) { list, task -> list + task }
+                .collect { allTasks -> actualTasks = allTasks }
+        }
+        assertEquals(expectedTasks.size, actualTasks.size)
+        expectedTasks.forEachIndexed { index, task ->
+            assertEquals(task, actualTasks[index])
+        }
+    }
+}
