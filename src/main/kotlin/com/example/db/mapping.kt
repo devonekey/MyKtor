df8ca6d0192c711f7ac6a7a3da6293ba8ea7b1c5
OneKey <devonekey@gmail.com>
2024-12-17 15:52:57 +0900

    feat: Task 테이블을 정의한 객체 추가
    feat: Task에 대한 테이블 정의

---
+++ src/main/kotlin/com/example/db/mapping.kt
@@ -0,0 +1,12 @@
+package com.example.db
+
+import org.jetbrains.exposed.dao.IntEntity
+import org.jetbrains.exposed.dao.IntEntityClass
+import org.jetbrains.exposed.dao.id.EntityID
+import org.jetbrains.exposed.dao.id.IntIdTable
+
+object TaskTable : IntIdTable("task") {
+    val name = varchar("name", 50)
+    val description = varchar("description", 50)
+    val priority = varchar("priority", 50)
+}
