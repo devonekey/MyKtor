f4c12edc4df211bf1e6a9badc6c99cc3cac54a11
OneKey <devonekey@gmail.com>
2024-12-17 17:48:37 +0900

    feat: 테스트 코드를 제외하고 기존에 사용하던 FakeTaskRepository 대신 PostgresTaskRepository를 사용하도록 교체

--- src/main/kotlin/com/example/Application.kt
+++ src/main/kotlin/com/example/Application.kt
@@ -1,6 +1,6 @@
 package com.example
 
-import com.example.model.FakeTaskRepository
+import com.example.model.PostgresTaskRepository
 import com.example.plugins.configureRouting
 import com.example.plugins.configureSerialization
 import com.example.plugins.configureSockets
@@ -10,7 +10,7 @@ import io.ktor.server.netty.EngineMain
 fun main(args: Array<String>) = EngineMain.main(args)
 
 fun Application.module() {
-    val repository = FakeTaskRepository()
+    val repository = PostgresTaskRepository()
 
     configureSerialization()
     configureRouting(repository)
