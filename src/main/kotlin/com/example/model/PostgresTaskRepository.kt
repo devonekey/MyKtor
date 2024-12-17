56287bd6e0771308b2828f8d49e3dc3e803e8e1a
OneKey <devonekey@gmail.com>
2024-12-17 17:12:28 +0900

    feat: PostgresRepository의 각 동작에 대한 코드 추가

--- src/main/kotlin/com/example/model/PostgresTaskRepository.kt
+++ src/main/kotlin/com/example/model/PostgresTaskRepository.kt
@@ -1,23 +1,46 @@
 package com.example.model
 
+import com.example.db.TaskDAO
+import com.example.db.TaskTable
+import com.example.db.daoToModel
+import com.example.db.suspendTransaction
+import org.jetbrains.exposed.sql.SqlExpressionBuilder.eq
+import org.jetbrains.exposed.sql.deleteWhere
+
 class PostgresTaskRepository : TaskRepository {
-    override suspend fun allTask(): List<Task> {
-        TODO("Not yet implemented")
+    override suspend fun allTask(): List<Task> = suspendTransaction {
+        TaskDAO
+            .all()
+            .map(::daoToModel)
     }
 
-    override suspend fun taskByPriority(priority: Priority): List<Task> {
-        TODO("Not yet implemented")
+    override suspend fun taskByPriority(priority: Priority): List<Task> = suspendTransaction {
+        TaskDAO
+            .find { TaskTable.priority eq priority.toString() }
+            .map(::daoToModel)
     }
 
-    override suspend fun taskByName(name: String): Task? {
-        TODO("Not yet implemented")
+    override suspend fun taskByName(name: String): Task? = suspendTransaction {
+        TaskDAO
+            .find { TaskTable.name eq name }
+            .limit(1)
+            .map(::daoToModel)
+            .firstOrNull()
     }
 
-    override suspend fun addTask(task: Task) {
-        TODO("Not yet implemented")
+    override suspend fun addTask(task: Task): Unit = suspendTransaction {
+        TaskDAO.new {
+            name = task.name
+            description = task.description
+            priority = task.priority.toString()
+        }
     }
 
-    override suspend fun removeTask(name: String): Boolean {
-        TODO("Not yet implemented")
+    override suspend fun removeTask(name: String): Boolean = suspendTransaction {
+        val deleted = TaskTable.deleteWhere {
+            TaskTable.name eq name
+        }
+
+        deleted == 1
     }
 }
