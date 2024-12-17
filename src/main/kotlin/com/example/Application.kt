f0ee03f0609c82ef1b4826351a5b37eccd4cc212
OneKey <devonekey@gmail.com>
2024-12-17 18:21:17 +0900

    feat: PostgreSQL과 연결되도록 구현

--- src/main/kotlin/com/example/Application.kt
+++ src/main/kotlin/com/example/Application.kt
@@ -1,6 +1,7 @@
 package com.example
 
 import com.example.model.PostgresTaskRepository
+import com.example.plugins.configureDatabases
 import com.example.plugins.configureRouting
 import com.example.plugins.configureSerialization
 import com.example.plugins.configureSockets
@@ -13,6 +14,7 @@ fun Application.module() {
     val repository = PostgresTaskRepository()
 
     configureSerialization()
+    configureDatabases()
     configureRouting(repository)
     configureSockets(repository)
 }
